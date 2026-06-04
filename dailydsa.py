337d923ef969cf8f8e6ab67d2d1767451d9b68d8
ComradeVijay <comradevijay93@gmail.com>
2026-06-04 10:50:29 +0530

    solved 27. Remove Element

--- dailydsa.py
+++ dailydsa.py
@@ -1,6 +1,7 @@
-# 26. Remove Duplicates from Sorted Array
-# Input: nums = [1,1,2]
-# Output: 2, nums = [1,2,_]
+
+#! 26. Remove Duplicates from Sorted Array
+#? Input: nums = [1,1,2]
+#? Output: 2, nums = [1,2,_]
 
 def removeDub(nums):
     left = 0
@@ -12,4 +13,16 @@ def removeDub(nums):
 
 # print(removeDub([1,1,2]))   
 
+#! 27. Remove Element
+#? Input: nums = [3,2,2,3], val = 3
+#? Output: 2, nums = [2,2,_,_]
 
+def removeElement(nums, val):     
+    l = 0
+    for r in range(len(nums)):
+        if nums[r] != val:
+            nums[l] = nums[r]
+            l += 1
+    return l
+
+# print(removeElement([3, 2, 2, 3], 3))
